864a7e3ad4d34d7dd4d7fcc99e9a9d3a1ad61093
Phongtruong613 <phongtruong613@gmail.com>
2022-03-22 17:32:55 +0700

    kkkk

---
+++ ngaythangnam.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{   int a, b, c;
+    printf("nhap ngay = ");scanf("%d",&a);
+     printf("nhap thang = ");scanf("%d",&b);
+     printf("nhap nam = ");scanf("%d",&c);
+     printf("%d/%d/%d",a,b,c);
+}
